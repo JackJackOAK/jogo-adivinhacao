af96b35ea05bfc74e40d55ef59105a8f3dc0cf10
Jackson Carvalho <88566797+JackJackOAK@users.noreply.github.com>
2026-07-09 19:05:20 -0300

    Update API_URL to new backend endpoint

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
-const API_URL = "jogo-adivinhacao-backend.vercel.app";
+const API_URL = "https://jogo-adivinhacao-rhci.onrender.com";
 const MODO_ALEATORIO = "🎲 Aleatório";
 
 export default function App() {
